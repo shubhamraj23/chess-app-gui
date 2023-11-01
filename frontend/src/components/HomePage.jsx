e5e4bbe7fe44c5e5536692da5d547bbc6a14559b
Shubham Raj Pandit <shubhamraj465@gmail.com>
2023-11-01 12:59:09 +0530

    Added Loading Spinner for slow connections

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -12,6 +12,7 @@ const HomePage = () => {
   const [errorState, setErrorState] = useState('hidden')
   const [errorMessage, setErrorMessage] = useState('')
   const [color, setColor] = useState('red')
+  const [loading, setLoading] = useState('hidden')
   
   // React states to capture the input entered by the user.
   const [loginID, setLoginID] = useState('')
@@ -52,6 +53,7 @@ const HomePage = () => {
     event.preventDefault()
     setErrorState('hidden')
     setErrorMessage('')
+    setLoading('')
     if (pageCondition === 'login') {
       const loginData = {
         userId: loginID,
@@ -59,11 +61,19 @@ const HomePage = () => {
       }
       axios.post('user/login', loginData)
         .then(() => {
+          setSignupID('')
+          setSignupPassword('')
+          setSignupName('')
           return navigate('/dashboard')
         })
         .catch((error) => {
           setErrorState('')
-          setErrorMessage(error.response.data.error)
+          if (error.response) {
+            setErrorMessage(error.response.data.error)
+          }
+          else {
+            setErrorMessage("Server Error. Please try again.")
+          }
         })
     }
     else {
@@ -84,73 +94,87 @@ const HomePage = () => {
         .catch((error) => {
           setErrorState('')
           setColor('red')
-          setErrorMessage(error.response.data.error)
+          if (error.response) {
+            setErrorMessage(error.response.data.error)
+          }
+          else {
+            setErrorMessage("Server Error. Please try again.")
+          }
         })
     }
+    setLoading('hidden')
   }
 
   return (
-    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
-      <div className="bg-white w-96 rounded-lg shadow-lg">
-        <div className="flex justify-between items-center px-4 py-3 border-b">
-          <button className={`px-4 py-2 font-semibold focus:outline-none ${loginButton}`} onClick={()=>setPageCondition('login')}>Login</button>
-          <button className={`px-4 py-2 font-semibold focus:outline-none ${signupButton}`} onClick={()=>setPageCondition('signup')}>Signup</button>
-        </div>
-        
-        <div className="p-6">
-          <div className={loginForm}>
-            <h2 className="text-2xl font-semibold text-gray-700">Login</h2>
+    <div className="">
+      <div className="bg-gray-100 flex items-center justify-center min-h-screen relative">
+        <div className="bg-white w-96 rounded-lg shadow-lg">
+          <div className="flex justify-between items-center px-4 py-3 border-b">
+            <button className={`px-4 py-2 font-semibold focus:outline-none ${loginButton}`} onClick={()=>setPageCondition('login')}>Login</button>
+            <button className={`px-4 py-2 font-semibold focus:outline-none ${signupButton}`} onClick={()=>setPageCondition('signup')}>Signup</button>
+          </div>
+          
+          <div className="p-6">
+            <div className={loginForm}>
+              <h2 className="text-2xl font-semibold text-gray-700">Login</h2>
 
-            <div className={`${errorState} bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mt-2`}>
-              <p><strong>{errorMessage}</strong></p>
-            </div>
-
-            <form className="mt-4" onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label className={labelClasses}>Username</label>
-                <input className={inputClasses} type="text" placeholder="Username" 
-                  value={loginID} onChange={(e) => setLoginID(e.target.value)} required />
+              <div className={`${errorState} bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mt-2`}>
+                <p><strong>{errorMessage}</strong></p>
               </div>
 
-              <div className="mb-4">
-                <label className={labelClasses}>Password</label>
-                <input className={inputClasses} type="password" placeholder="Password"
-                  value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} required />
+              <form className="mt-4" onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className={labelClasses}>Username</label>
+                  <input className={inputClasses} type="text" placeholder="Username" 
+                    value={loginID} onChange={(e) => setLoginID(e.target.value)} required />
+                </div>
+
+                <div className="mb-4">
+                  <label className={labelClasses}>Password</label>
+                  <input className={inputClasses} type="password" placeholder="Password"
+                    value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} required />
+                </div>
+                
+                <button className={buttonClasses}>Login</button>
+              </form>
+            </div>
+
+            <div className={signupForm}>
+              <h2 className="text-2xl font-semibold text-gray-700">Signup</h2>
+
+              <div className={`${errorState} bg-${color}-100 border-l-4 border-${color}-500 text-${color}-700 p-4 mt-2`}>
+                <p><strong>{errorMessage}</strong></p>
               </div>
               
-              <button className={buttonClasses}>Login</button>
-            </form>
-          </div>
+              <form className="mt-4" onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className={labelClasses}>Name</label>
+                  <input className={inputClasses} type="text" placeholder="Name"
+                    value={signupName} onChange={(e) => setSignupName(e.target.value)} required />
+                </div>
+                      
+                <div className="mb-4">
+                  <label className={labelClasses}>Username</label>
+                  <input className={inputClasses} type="text" placeholder="Username"
+                    value={signupID} onChange={(e) => setSignupID(e.target.value)} required />
+                </div>
+                      
+                <div className="mb-4">
+                  <label className={labelClasses}>Password</label>
+                  <input className={inputClasses} type="password" placeholder="Password"
+                    value={signupPassword} onChange={(e) => setSignupPassword(e.target.value)} required />
+                </div>
 
-          <div className={signupForm}>
-            <h2 className="text-2xl font-semibold text-gray-700">Signup</h2>
-
-            <div className={`${errorState} bg-${color}-100 border-l-4 border-${color}-500 text-${color}-700 p-4 mt-2`}>
-              <p><strong>{errorMessage}</strong></p>
+                <button className={buttonClasses}>Signup</button>
+              </form>
             </div>
-            
-            <form className="mt-4" onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label className={labelClasses}>Name</label>
-                <input className={inputClasses} type="text" placeholder="Name"
-                  value={signupName} onChange={(e) => setSignupName(e.target.value)} required />
-              </div>
-                    
-              <div className="mb-4">
-                <label className={labelClasses}>Username</label>
-                <input className={inputClasses} type="text" placeholder="Username"
-                  value={signupID} onChange={(e) => setSignupID(e.target.value)} required />
-              </div>
-                    
-              <div className="mb-4">
-                <label className={labelClasses}>Password</label>
-                <input className={inputClasses} type="password" placeholder="Password"
-                  value={signupPassword} onChange={(e) => setSignupPassword(e.target.value)} required />
-              </div>
-
-              <button className={buttonClasses}>Signup</button>
-            </form>
           </div>
+        </div>
+      </div>
+
+      <div className={`${loading} absolute top-0`}>
+        <div className="h-screen w-screen bg-gray-100 bg-opacity-50 flex items-center justify-center">
+          <div className="w-16 h-16 border-t-4 border-blue-500 rounded-full animate-spin"></div>
         </div>
       </div>
     </div>
