967cd7a2ab3400c35d228c2d507ae5cd3598c40c
Shubham Raj Pandit <shubhamraj465@gmail.com>
2023-12-20 20:08:30 +0530

    Added test cases for spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,8 +1,8 @@
 const Spinner = ({status, text}) => {
   return (
-      <div className={`${status} absolute top-0`}>
+      <div className={`${status} absolute top-0`} data-testid="spinner">
         <div className="h-screen w-screen bg-gray-100 bg-opacity-100 flex flex-col items-center justify-center">
-          <div className="w-16 h-16 border-t-4 border-blue-500 rounded-full animate-spin"></div>
+          <div className="w-16 h-16 border-t-4 border-blue-500 rounded-full animate-spin" data-testid="animation"></div>
           <div className="left-0">
             {text && <p className="mt-4 text-gray-1000">{text}</p>}
           </div>
---
+++ frontend/src/tests/spinner.test.js
@@ -0,0 +1,47 @@
+import '@testing-library/jest-dom/extend-expect'
+import {render, screen} from '@testing-library/react'
+import Spinner from '../components/Spinner'
+
+describe('Spinner Component', () => {
+  test('Render Spinner without any text', () => {
+    // Render the spinner on the screen.
+    render(<Spinner status='' />)
+
+    // Check that the spinner has been rendered without any text.
+    expect(screen.getByTestId('spinner')).toBeInTheDocument()
+    expect(screen.queryByText('Loading')).toBeNull()
+  })
+
+  test('Render Spinner with text', () => {
+    // Render the spinner on the screen.
+    render(<Spinner status='' text='Loading...'/>)
+
+    // Check that the spinner has been rendered with the correct text.
+    expect(screen.getByTestId('spinner')).toBeInTheDocument()
+    expect(screen.getByText('Loading...')).toBeInTheDocument()
+  })
+
+  test('Check for appropriate classes in the component', () => {
+    // Render the spinner on the screen.
+    render(<Spinner status='' text='Loading...'/>)
+
+    // Check that the spinner container has the correct classes.
+    expect(screen.getByTestId('spinner')).toBeInTheDocument()
+    const spinnerContainer = screen.getByTestId('spinner')
+    expect(spinnerContainer).toHaveClass('absolute top-0')
+    
+    expect(screen.getByTestId('animation')).toBeInTheDocument()
+    const animationContainer = screen.getByTestId('animation')
+    expect(animationContainer).toHaveClass('rounded-full animate-spin')
+  })
+
+  test('Check for hidden spinner', () => {
+    // Render the spinner on the screen.
+    render(<Spinner status='hidden' text='Loading...'/>)
+
+    // Check that the spinner container has been rendered and has the correct classes.
+    expect(screen.getByTestId('spinner')).toBeInTheDocument()
+    const spinnerContainer = screen.getByTestId('spinner')
+    expect(spinnerContainer).toHaveClass('hidden')
+  })
+})
