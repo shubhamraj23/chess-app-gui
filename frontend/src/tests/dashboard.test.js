1ab45b0e6ec8d2bd1982a93047b4685214f68e51
Shubham Raj Pandit <shubhamraj465@gmail.com>
2024-01-18 19:14:47 +0530

    Added test cases for matching button click

--- frontend/src/tests/dashboard.test.js
+++ frontend/src/tests/dashboard.test.js
@@ -1,12 +1,14 @@
 import '@testing-library/jest-dom/extend-expect'
 import { BrowserRouter as Router } from 'react-router-dom'
-import {render, screen, fireEvent, waitFor} from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
 import axios from 'axios'
+import io from 'socket.io-client'
 import DashBoard from '../components/DashBoard'
 
 // Mock axios
 jest.mock('axios')
+jest.mock('socket.io-client')
 
 // Mock useNavigate
 const mockNavigate = jest.fn()
@@ -120,9 +122,9 @@ describe('DashBoard Component', () => {
     expect(screen.getByTestId('won')).toBeInTheDocument()
     
     // Check appropriate data to present in the fields.
-    expect(screen.getByText(/Welcome, SRP!/)).toBeInTheDocument()
-    expect(screen.getByText(/Total Games Played: 2/)).toBeInTheDocument()
-    expect(screen.getByText(/Total Games Won: 1/)).toBeInTheDocument()
+    expect(screen.getByTestId('name')).toHaveTextContent('Welcome, SRP!')
+    expect(screen.getByTestId('games')).toHaveTextContent('Total Games Played: 2')
+    expect(screen.getByTestId('won')).toHaveTextContent('Total Games Won: 1')
   })
 
 
@@ -152,10 +154,11 @@ describe('DashBoard Component', () => {
     expect(screen.getByTestId('won')).toBeInTheDocument()
     
     // Check appropriate data to present in the fields.
-    expect(screen.getByText(/Welcome, User!/)).toBeInTheDocument()
-    expect(screen.getByText(/Total Games Played: 0/)).toBeInTheDocument()
-    expect(screen.getByText(/Total Games Won: 0/)).toBeInTheDocument()
+    expect(screen.getByTestId('name')).toHaveTextContent('Welcome, User!')
+    expect(screen.getByTestId('games')).toHaveTextContent('Total Games Played: 0')
+    expect(screen.getByTestId('won')).toHaveTextContent('Total Games Won: 0')
   })
+
 
   test('Spinner visible on logout button click', async () => {
     // Mock axios get routes to be resolved.
@@ -185,10 +188,11 @@ describe('DashBoard Component', () => {
     expect(screen.getByTestId('spinner-normal')).not.toHaveClass('hidden')
   })
 
+
   test('Spinner visible on match button click', async () => {
     // Mock axios get routes to be resolved.
     axios.get.mockResolvedValue()
-    axios.post.mockResolvedValue()
+    axios.post.mockRejectedValue(new Error())
 
     // Render the DashBoard component on the screen.
     // eslint-disable-next-line testing-library/no-unnecessary-act
@@ -203,7 +207,7 @@ describe('DashBoard Component', () => {
     // Check that the dashboard component has been rendered.
     expect(screen.getByTestId('dashboard')).toBeInTheDocument()
 
-    // Click on the logout button.
+    // Click on the matching button.
     // eslint-disable-next-line testing-library/no-unnecessary-act
     await act(async () => {
       fireEvent.click(screen.getByTestId('button'))
@@ -211,6 +215,104 @@ describe('DashBoard Component', () => {
     
     // Check that the spinner is visible.
     expect(screen.getByTestId('spinner-text')).not.toHaveClass('hidden')
-    expect(screen.getByText(/Finding a match for you/)).toBeInTheDocument()
+    expect(screen.getByTestId('spinner-text')).toHaveTextContent('Finding a match for you')
+  })
+
+
+  test('Error state on invalid post request', async () => {
+    // Mock axios get routes to be resolved.
+    axios.get.mockResolvedValue()
+    axios.post.mockRejectedValue({
+      response: {
+        data: {
+          error: 'Some random error.'
+        }
+      }
+    })
+
+    // Render the DashBoard component on the screen.
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => {
+      render(
+        <Router>
+          <DashBoard />
+        </Router>
+      )
+    })
+
+    // Check that the dashboard component has been rendered.
+    expect(screen.getByTestId('dashboard')).toBeInTheDocument()
+
+    // Check the error class has been rendered.
+    const error = screen.getByTestId('error')
+    expect(error).toBeInTheDocument()
+    expect(error).toHaveClass('hidden')
+
+    // Click on the matching button.
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => {
+      fireEvent.click(screen.getByTestId('button'))
+    })
+
+    // Expect axios post request has been called.
+    expect(axios.post).toHaveBeenCalledWith('/match')
+    
+    // Ensure the error comes after axios request is rejected.
+    await waitFor(() => {
+      expect(error).not.toHaveClass('hidden')
+      // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
+      expect(error).toHaveTextContent('Some random error.')
+    })
+  })
+
+
+  test('Rerouting on valid post request', async () => {
+    // Mock axios get routes to be resolved.
+    axios.get.mockResolvedValue()
+    axios.post.mockResolvedValue({ data: 'Some random data' })
+
+    // Mock the Socket.IO methods
+    const onMock = jest.fn()
+    const emitMock = jest.fn()
+
+    // Mock the socket instance and methods
+    const socketMock = {
+      on: onMock,
+      emit: emitMock,
+      disconnect: jest.fn()
+    }
+    io.mockReturnValue(socketMock)
+
+    // Render the DashBoard component on the screen.
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => {
+      render(
+        <Router>
+          <DashBoard />
+        </Router>
+      )
+    })
+
+    // Check that the dashboard component has been rendered.
+    expect(screen.getByTestId('dashboard')).toBeInTheDocument()
+
+    // Click on the matching button.
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => {
+      fireEvent.click(screen.getByTestId('button'))
+    })
+
+    // Expect axios post request has been called.
+    expect(axios.post).toHaveBeenCalledWith('/match')
+    
+    // Simulate the socket event for 'match-found'
+    const mockMatchData = { id: 'mockGameId' }
+    await act(async () => {
+      onMock.mock.calls[0][1](mockMatchData)
+    })
+
+    // Assert that socket.emit and navigate were called
+    expect(emitMock).toHaveBeenCalledWith('match-request', { data: 'Some random data'})
+    expect(mockNavigate).toHaveBeenCalledWith(`/game/${mockMatchData.id}`)
   })
 })
