4f3205ec31488f215ace6d246bbcc7d1b04ee2df
Shubham Raj Pandit <shubhamraj465@gmail.com>
2024-02-21 17:06:16 +0530

    Bug Fix on cursor type

--- frontend/src/components/ChessCell.jsx
+++ frontend/src/components/ChessCell.jsx
@@ -7,12 +7,18 @@ const ChessCell = ({children, isDark, width, type, player, turn}) => {
     if (type && turn) {
       const prefix = (player === 0) ? 'white' : 'black'
       if (type.startsWith(prefix)) setCursor('cursor-pointer')
+      else setCursor('')
     }
-  }, [])
+    else setCursor('')
+  }, [player, type, turn])
+
+  const generateMoves = () => {
+    if (cursor !== 'cursor-pointer') return
+  }
 
   return (
     <div className={`flex items-center justify-center ${isDark ? 'dark-cell' : 'light-cell'} ${cursor}`} 
-      style={{ width: `${width}px`, height: `${width}px` }}>
+      style={{ width: `${width}px`, height: `${width}px` }} onClick={generateMoves}>
         {children}
     </div>
   )
