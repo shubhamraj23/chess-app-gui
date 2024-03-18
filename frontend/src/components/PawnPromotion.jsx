bc8950b4f976c5e9e80e6c1a55b4e7a260e7b8bb
Shubham Raj Pandit <shubhamraj465@gmail.com>
2024-03-18 20:45:21 +0530

    Added a cross button to go back

--- frontend/src/components/PawnPromotion.jsx
+++ frontend/src/components/PawnPromotion.jsx
@@ -6,7 +6,7 @@ const PawnPromotion = ({ width, player }) => {
   
   return (
     <div className="z-10 absolute top-0 flex flex-row items-center">
-      <div className="my-auto" style={{ width: `${width}px`, height: `${width * 4.5}px` }}>
+      <div className="my-auto" style={{ width: `${width}px`, height: `${width * 4.3}px` }}>
         {options.map((cell, index) => (
           <div key={index}>
             <div className={`relative flex items-center justify-center bg-white cursor-pointer`} 
@@ -15,6 +15,13 @@ const PawnPromotion = ({ width, player }) => {
             </div>
           </div>
         ))}
+
+        <div className='bg-white flex justify-center items-center' style={{ width: `${width}px`, height: `${0.3 * width}px` }}>
+          <div className="flex items-center justify-center border border-red-500 rounded-full"
+            style={{ width: `${0.25 * width}px`, height: `${0.25 * width}px` }}>
+              <span className="text-red-500">&#10060;</span>
+          </div>
+        </div>
       </div>
     </div>
   )
